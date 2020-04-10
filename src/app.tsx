f8c6b2b37c89ebc40f17ef6e8225a2c6cfde5f84
hezhenfeng <feng960106@163.com>
2020-04-10 18:53:16 +0800

    晚上页面

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import Taro, {Component, Config} from '@tarojs/taro'
 import {Provider} from '@tarojs/redux';
 import store from './utils/dva';
 import './app.scss'
-
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
